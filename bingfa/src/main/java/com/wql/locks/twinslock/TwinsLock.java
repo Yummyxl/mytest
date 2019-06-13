d4ded593660566ccc2a8c285d2c255eabbf834af
BJHL <bjhl@Yummyxl.local>
2019-06-13 15:26:51 +0800

    add

--- bingfa/src/main/java/com/wql/locks/twinslock/TwinsLock.java
+++ bingfa/src/main/java/com/wql/locks/twinslock/TwinsLock.java
@@ -1,6 +1,7 @@
 package com.wql.locks.twinslock;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 
@@ -14,9 +15,41 @@ import java.util.concurrent.locks.Lock;
 
 public class TwinsLock implements Lock {
 
+    private final Sync sync = new Sync(3);
+    private static final class Sync extends AbstractQueuedSynchronizer {
+        public Sync(int count) {
+            if (count <= 0) {
+                throw new IllegalArgumentException("count must more than 0");
+            }
+            setState(count);
+        }
+
+        @Override
+        protected int tryAcquireShared(int count) {
+            for(;;) {
+                int current = getState();
+                int newCount = current - count;
+                if (newCount < 0 || compareAndSetState(current, newCount)) {
+                    return newCount;
+                }
+            }
+        }
+
+        @Override
+        protected boolean tryReleaseShared(int count) {
+            for (;;) {
+                int current = getState();
+                int newCount = current + count;
+                if (compareAndSetState(current, newCount)) {
+                    return true;
+                }
+            }
+        }
+    }
+
     @Override
     public void lock() {
-        
+        sync.acquireShared(1);
     }
 
     @Override
@@ -36,11 +69,54 @@ public class TwinsLock implements Lock {
 
     @Override
     public void unlock() {
-
+        sync.releaseShared(1);
     }
 
     @Override
     public Condition newCondition() {
         return null;
+    }
+
+    public static void main(String[] args) {
+        final TwinsLock lock = new TwinsLock();
+
+        class Worker extends Thread {
+            @Override
+            public void run() {
+                while (true) {
+                    lock.lock();
+                    try {
+                        try {
+                            TimeUnit.SECONDS.sleep(1);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println(Thread.currentThread().getName());
+                        try {
+                            TimeUnit.SECONDS.sleep(1);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    } finally {
+                        lock.unlock();
+                    }
+                }
+            }
+        }
+
+        for (int i=0; i<100; i++) {
+            Worker r = new Worker();
+            r.setDaemon(true);
+            r.start();
+        }
+
+        for (int i=0; i<10; i++) {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println();
+        }
     }
 }
