c543d1ab76ee813962a2c198aa4620548de1f83c
Yummyxl <huangfu_jx@163.com>
2019-09-30 13:08:17 +0800

    停止线程

--- bingfa/src/main/java/com/wql/concurrency/HowToStopThreadQuestion.java
+++ bingfa/src/main/java/com/wql/concurrency/HowToStopThreadQuestion.java
@@ -10,7 +10,33 @@ package com.wql.concurrency;
 
 public class HowToStopThreadQuestion {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
+        Action action = new Action();
+        Thread t1 = new Thread(action, "t1");
+        t1.start();
+        action.setStoped(true);
+        t1.join();
+    }
+
+    public static class Action implements Runnable {
+
+        // 线程安全问题，确保可见性 (happen-before)
+        private volatile boolean stoped = false;
+
+        @Override
+        public void run() {
+            if (!stoped) {
+                access();
+            }
+        }
+
+        public void setStoped(boolean stoped) {
+            this.stoped = stoped;
+        }
+    }
+
+    private static void access() {
+        System.out.printf("[%s] 线程正在执行....\n", Thread.currentThread().getName());
     }
 }
